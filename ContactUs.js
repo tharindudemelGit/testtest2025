7b54aac0e7a3cd045bce0f83dd4926032b7a66fb
Your Name <tharindu.demel2@gmail.com>
2025-09-25 21:00:25 +0530

    first commit

--- ContactUs.js
+++ ContactUs.js
@@ -7,7 +7,7 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Contact Us' }).click();
   await page.getByRole('textbox', { name: 'Name *' }).click();
 
-// Fill test user
+// Fill test user //
   await page.getByRole('textbox', { name: 'Name *' }).fill('Test User');
   await page.getByRole('textbox', { name: 'Email Address *' }).click();
 // Fill email address
@@ -16,7 +16,7 @@ test('test', async ({ page }) => {
 // Fill text message
   await page.locator('textarea[name="your-message"]').fill('This is a test message');
 
-// Assert the text that entered
+// Assert the text that entered //
   await expect(page.getByRole('textbox', { name: 'Name *' })).toHaveValue('Test User');
   await expect(page.getByLabel('Contact form')).toContainText('Name Email Address Phone CompanyPlease fill the mandatory fields. Message.... Submit');
   await expect(page.locator('textarea[name="your-message"]')).toHaveValue('This is a test message');
